a7046e87451d63a7537bff871eb0f8edca9ec957
Lorenzo <lorenzosimonassi@gmail.com>
2026-08-06 11:05:47 -0300

    test: add vue plugin and alias to vitest for component specs

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,9 @@
 const path = require('node:path')
+const vue = require('@vitejs/plugin-vue')
 const { defineConfig } = require('vitest/config')
 
 module.exports = defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: {
       src: path.resolve(__dirname, 'src'),
